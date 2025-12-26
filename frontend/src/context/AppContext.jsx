cc85179af25e3a761b9ff2b450728920a23e20da
sunny-rp <sunnyprajapar65351@gmail.com>
2025-12-26 16:09:55 +0530

    Enhance CourseDetails component: integrate chapter and course duration calculations, add total lectures count, and improve layout with responsive design

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from "humanize-duration";
 
 export const AppContext = createContext();
 
@@ -28,12 +29,40 @@ export const AppContextProvider = ({ children }) => {
     return totalRating / course.courseRatings.length;
   }
 
+  // function to Calculate Course Chapter Time
+    const calculateChapterTime = (chapter) => {
+      let time =0;
+      chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration);
+      return humanizeDuration(time * 60 * 1000, { units: ['h', 'm']});
+    }
+
+    //Function to Calculate Course Duration
+   const calculateCourseDuration = (course) => {
+      let time =0;
+
+      course.courseContent.map((chapter)=> chapter.chapterContent.map((lecture)=> time += lecture.lectureDuration));
+      return humanizeDuration(time * 60 * 1000, { units: ['h', 'm']});
+   }
+
+   //Function to Calculate Total Lectures in a Course
+    const calculateNoOfLectures = (course) => {
+      let totalLectures = 0;
+      course.courseContent.forEach(chapter => {
+        if(Array.isArray(chapter.chapterContent)){
+          totalLectures += chapter.chapterContent.length;
+        }
+      })
+
+      return totalLectures
+    }
+
+
   useEffect(() => {
     fetchAllCourses();
   }, []);
   const value = {
     // Define any shared state or functions here
-    currency, allcourses, navigate, calculateRating ,isEducator, setIsEducator
+    currency, allcourses, navigate, calculateRating ,isEducator, setIsEducator ,calculateChapterTime, calculateCourseDuration, calculateNoOfLectures  
   };
   return (
     <AppContext.Provider value={value}>
